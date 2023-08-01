f72f98807104cad03e4bfc104b505fd2e338e766
Patrick Kosewe <patrick.kosewe@expertflow.com>
2023-08-01 11:36:19 +0300

    cards api commit

--- src/main/java/com/logicea/cards/CardsApplication.java
+++ src/main/java/com/logicea/cards/CardsApplication.java
@@ -1,5 +1,7 @@
 package com.logicea.cards;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,12 +9,15 @@ import java.util.Properties;
 
 @SpringBootApplication
 public class CardsApplication {
+    public static Logger logger = LogManager.getLogger("com.logicea.cards");
 
+    public static String appVersion = "1.0";
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(new Class[]{CardsApplication.class});
         Properties properties = new Properties();
         properties.put("server.port", 8088);
         application.setDefaultProperties(properties);
+        logger.debug("Starting Cards API with version:" + appVersion);
         application.run(args);
     }
 
